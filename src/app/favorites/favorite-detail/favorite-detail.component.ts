94f74cddcf3fb2528bd50c5a52287e2768c4cf02
Familia de Francisco Rodriguez <familiadefranciscorodriguez@iMac-de-Familia.local>
2017-03-09 23:52:24 +0100

    Favorite details added

--- src/app/favorites/favorite-detail/favorite-detail.component.ts
+++ src/app/favorites/favorite-detail/favorite-detail.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router'
 
+import { FavoriteService } from '../favorite.service'
+import { Favorite } from '../favorite'
+
 @Component({
   selector: 'app-favorite-detail',
   templateUrl: './favorite-detail.component.html',
-  styleUrls: ['./favorite-detail.component.css']
+  styleUrls: ['./favorite-detail.component.css'],
+  providers: [ FavoriteService ]
 })
 
 export class FavoriteDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _favoriteService: FavoriteService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.getFavorite()
   }
 
+  getFavorite() {
+    this._route.params.forEach((params: Params) => {
+      let idFavorite = params['id']
+      this._favoriteService.getFavorito(idFavorite).subscribe(
+        response => {
+
+        },
+        error => {
+          
+        }
+      )
+    })
+  }
 }
